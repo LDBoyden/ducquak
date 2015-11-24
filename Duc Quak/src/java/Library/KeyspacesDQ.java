dedd1c985655d8b90a846c29a0a0cf562f347546
NourSerw <nourserw@hotmail.com>
2015-11-24 14:53:00 +0000

    Test stuff

--- Duc Quak/src/java/Library/KeyspacesDQ.java
+++ Duc Quak/src/java/Library/KeyspacesDQ.java
@@ -5,10 +5,26 @@
  */
 package Library;
 
+import org.apache.catalina.Cluster;
+
 /**
  *
  * @author NSERW
  */
 public class KeyspacesDQ {
     
+    public KeyspacesDQ()
+    {
+        
+    }
+    
+    public static void setUpKeyspace(Cluster c)
+    {
+        String createKeySpace = "create keyspace if not exists ducQuak  WITH replication = {'class':'SimpleStrategy', 'replication_factor':1}";
+        String createanonymousUsers = "CREATE TABLE if not exists ducQuak.users ("
+                + "userID uuid, "
+                + "points int,"
+                + " PRIMARY KEY (userID)"
+                + ")";
+    }
 }
