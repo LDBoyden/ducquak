6e3d8ae6a3039d234fb52cf1b0cd2038a0b30531
azpopov <arturpopov@labpc123.computing.dundee.ac.uk>
2015-11-27 13:45:39 +0000

    123

--- Ducquak/src/main/java/Library/keyspacesDQ.java
+++ Ducquak/src/main/java/Library/keyspacesDQ.java
@@ -52,7 +52,7 @@ public final class keyspacesDQ {
             String userThreads = "CREATE TABLE if not exists ducquak.userthreads ("
                     + "userName text,"
                     + "threadID uuid,"
-                    + "PRIMARY KEY (userName)"
+                    + "PRIMARY KEY (userName, threadID)"
                     + ")";
             String userThreadsIndex = "CREATE INDEX threadIndex ON ducquak.usersthreads(threadID);";
             Session session = c.connect();
